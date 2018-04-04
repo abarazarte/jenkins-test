39d4d2abc701c210e24c2e646dc940b49073c000
Alejandro Barazarte <alejandrobarazarte@hotmail.com>
2018-04-04 17:18:03 -0300

    Obtiene la version desde el pom

--- app/api-users/run.sh
+++ app/api-users/run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-NAME=`mvn help:evaluate -Dexpression=project.name | grep "^[^\[]"`
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
-java -jar target/${NAME}-${VERSION}.jar --server.port=3100 &
+java -jar target/api-users-${VERSION}.jar --server.port=3100 &
