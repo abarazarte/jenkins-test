0033d74974cd7a6cdf440e02898bd6802c500f13
Alejandro Barazarte <alejandrobarazarte@hotmail.com>
2018-04-04 16:35:27 -0300

    Modifica version en el run

--- app/api-users/run.sh
+++ app/api-users/run.sh
@@ -2,4 +2,4 @@
 
 kill $(lsof -i :3100 | grep LISTEN | awk '{print $2}')
 BUILD_ID=do_not_kill_me
-java -jar target/api-users-1.0.0.jar --server.port=3100 &
+java -jar target/api-users-1.0.2.jar --server.port=3100 &
