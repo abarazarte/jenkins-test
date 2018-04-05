08f508570b0c0d42f8228118fa94cf0bf9937f72
Alejandro Barazarte <alejandrobarazarte@hotmail.com>
2018-04-05 11:50:31 -0300

    A

--- app/api-users/run.sh
+++ app/api-users/run.sh
@@ -3,5 +3,5 @@ kill -9 $(lsof -i:3100 -t) 2> /dev/null
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 echo ${VERSION}
 BUILD_ID=do_not_kill_me
-java -jar target/api-users-${VERSION}.jar --server.port=3100
+java -jar target/api-users-${VERSION}.jar --server.port=3100 &
 echo "Executing api-users-${VERSION}.jar"
