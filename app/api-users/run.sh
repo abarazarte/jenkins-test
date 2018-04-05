a4782ea5278e3c635db8dfa2ec0b8e2af9a81278
Alejandro Barazarte <alejandrobarazarte@hotmail.com>
2018-04-05 12:52:50 -0300

    Modifica run script

--- app/api-users/run.sh
+++ app/api-users/run.sh
@@ -2,7 +2,7 @@
 echo 'Deploying spreingboot'
 VERSION=`mvn help:evaluate -Dexpression=project.version | grep "^[^\[]"`
 echo ${VERSION}
-BUILD_ID=do_not_kill_me
 echo "Executing api-users-${VERSION}.jar"
-java -jar target/api-users-${VERSION}.jar --server.port=3100 &
-exit
+BUILD_ID=do_not_kill_me
+nohup java -jar target/api-users-${VERSION}.jar --server.port=3100 &
+sleep 1
